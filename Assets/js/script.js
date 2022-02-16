9bfbffe6be5344f6d785e0bfa26fc77efac0335f
LilTzar <eeoerkeedu@gmail.com>
2022-02-15 19:00:58 -0700

    Ssetting up initial JS code, adjusting html and CSS to match changes made fro JS

---
+++ Assets/js/script.js
@@ -0,0 +1,30 @@
+//getting element variables set
+
+var scoreboard = document.getElementById("scoreblock");
+var quizBox = document.getElementById("quizblock");
+
+//defining variables
+var time = 0;
+var score = 0;
+var userName = "";
+
+//user profile to track stats
+var userProfile = {
+  Name: userName,
+  userScore: score,
+  userTime: 10000,
+};
+
+//questions to be asked
+var questions = [];
+
+// Button to hide scoreboard, initiate test, and start timer
+
+// intiate page load
+function initiate() {
+  //hide quiz
+  //stop quiz
+  //show scoreboard -- user best
+}
+
+initiate();
