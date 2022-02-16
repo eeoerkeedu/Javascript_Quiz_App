67a4c880e9346dbeeb93545ca5344e96d3dac77b
LilTzar <eeoerkeedu@gmail.com>
2022-02-15 20:05:31 -0700

    countdown timer added, more variables to get html assets, and attaching some funtions to buttons for testing

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,12 @@
 //getting element variables set
-
-var scoreboard = document.getElementById("scoreblock");
-var quizBox = document.getElementById("quizblock");
+var timerEL = document.getElementById("timer");
+var scoreBoardEL = document.getElementById("scoreblock");
+var quizBoxEL = document.getElementById("quizblock");
+var quizStartButtonEL = document.getElementById("startquiz");
+var scoreBoardNavEL = document.getElementById("highscores");
 
 //defining variables
-var time = 0;
+var TimeRemaining = 0;
 var score = 0;
 var userName = "";
 
@@ -12,19 +14,52 @@ var userName = "";
 var userProfile = {
   Name: userName,
   userScore: score,
-  userTime: 10000,
+  userTime: TimeRemaining,
 };
 
 //questions to be asked
 var questions = [];
 
+// funtion used by begin quiz button, starts countdown before timer begins, and loads question display function
+function beginQuiz() {
+  //displays the quiz and hide the scoreboard
+  scoreBoardEL.style.display = "none";
+  quizBoxEL.style.display = "block";
+  TimeRemaining = 1;
+  score = [];
+  window.alert("Beginning Quiz" + "\n" + "Watch your time...");
+
+  var timeLeft = 4;
+  var countDown = setInterval(function () {
+    // displayMessage();
+    timeLeft--;
+    timerEL.textContent = "Get ready, " + timeLeft;
+
+    if (timeLeft === 0) {
+      clearInterval(countDown);
+      displayMessage();
+      timerEl.textContent = null;
+    }
+  }, 1000);
+
+  //start timer
+  //display question -contains buttons
+}
+
 // Button to hide scoreboard, initiate test, and start timer
 
 // intiate page load
-function initiate() {
+function showScores() {
   //hide quiz
+  quizBoxEL.style.display = "none";
   //stop quiz
+
   //show scoreboard -- user best
+  scoreBoardEL.style.display = "block";
 }
 
-initiate();
+quizStartButtonEL.addEventListener("click", beginQuiz);
+
+scoreBoardNavEL.addEventListener("click", showScores);
+
+showScores();
